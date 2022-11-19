870c1b0b901bb109da9a37576b0948e7491d9bcb
Yongle Xu <yonglexu08@gmail.com>
2022-11-19 14:48:37 +0000

    can retrieve google calendar events and refresh oauth2 token when expired. Token stored in bonzai config and client stored as embeded file.

--- cmd.go
+++ cmd.go
@@ -1,20 +1,33 @@
 package routine
 
 import (
-	"encoding/json"
+	"context"
+	"embed"
 	"fmt"
 	"log"
+	"net/http"
+	"time"
 
 	Z "github.com/rwxrob/bonzai/z"
 	"github.com/rwxrob/conf"
+	"github.com/rwxrob/emb"
 	"github.com/rwxrob/help"
 	"github.com/rwxrob/vars"
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
+	"google.golang.org/api/calendar/v3"
+	"google.golang.org/api/option"
 	"gopkg.in/yaml.v3"
 )
+
+//go:embed files/*
+var files embed.FS
 
 func init() {
 	Z.Conf.SoftInit()
 	Z.Vars.SoftInit()
+	emb.FS = files
+	emb.Top = "files"
 }
 
 var Cmd = &Z.Cmd{
@@ -31,7 +44,7 @@ var Cmd = &Z.Cmd{
 	Commands: []*Z.Cmd{
 		printCmd,
 		// standard external branch imports (see rwxrob/{help,conf,vars})
-		help.Cmd, conf.Cmd, vars.Cmd,
+		help.Cmd, conf.Cmd, vars.Cmd, emb.Cmd,
 	},
 
 	// Add custom BonzaiMark template extensions (or overwrite existing ones).
@@ -42,34 +55,105 @@ var Cmd = &Z.Cmd{
 }
 
 var printCmd = &Z.Cmd{
-	Name:     `daily`,
-	Summary:  `print current to standard output (default)`,
+	Name:     `today`,
+	Summary:  `print today routine to standard output (default)`,
 	Commands: []*Z.Cmd{help.Cmd},
 	Call: func(x *Z.Cmd, _ ...string) error {
-		val, _ := Z.Conf.Data()
-		if val == "" {
-			log.Printf("API key has not been initialized. Please set the API key by using routine conf edit")
+		ctx := context.Background()
+
+		jsn, _ := emb.Cat("credentials2.json")
+		// If modifying these scopes, delete your previously saved token.json.
+		config, err := google.ConfigFromJSON(jsn, calendar.CalendarReadonlyScope)
+		if err != nil {
+			log.Fatalf("Unable to parse client secret file to config: %v", err)
+		}
+		client := getClient(config)
+
+		srv, err := calendar.NewService(ctx, option.WithHTTPClient(client))
+
+		if err != nil {
+			log.Printf("Unable to retrieve Calendar client: %v", err)
 			return nil
 		}
-
-		jsn, _ := conf2Json([]byte(val))
-
-		fmt.Println(string(jsn))
-
+		//Time min
+		min := time.Now().Format(time.RFC3339)
+		//max := time.Date(2022, time.November, 24, 0, 0, 0, 0, time.Local).Format(time.RFC3339)
+		events, err := srv.Events.List("primary").SingleEvents(true).TimeMin(min).MaxResults(3).Do()
+		if err != nil {
+			log.Printf("Unable to retrieve next ten of the user's events: %v", err)
+			return nil
+		}
+		fmt.Println("Upcoming events:")
+		if len(events.Items) == 0 {
+			fmt.Println("No upcoming events found.")
+		} else {
+			for _, item := range events.Items {
+				date := item.Start.DateTime
+				if date == "" {
+					date = item.Start.Date
+				}
+				fmt.Printf("%v (%v)\n", item.Summary, date)
+			}
+		}
 		return nil
 	},
 }
 
-func conf2Json(body []byte) ([]byte, error) {
-	m := make(map[string]any)
-	err := yaml.Unmarshal(body, &m)
+// Retrieve a token, saves the token, then returns the generated client.
+func getClient(config *oauth2.Config) *http.Client {
+	tok, err := tokenFromFile(config)
 	if err != nil {
-		return nil, err
+		tok = getTokenFromWeb(config)
+		saveToken(tok)
+	}
+	return config.Client(context.Background(), tok)
+}
+
+// Request a token from the web, then returns the retrieved token.
+func getTokenFromWeb(config *oauth2.Config) *oauth2.Token {
+	authURL := config.AuthCodeURL("state-token", oauth2.AccessTypeOffline)
+	fmt.Printf("Go to the following link in your browser then type the "+
+		"authorization code: \n%v\n", authURL)
+
+	var authCode string
+	if _, err := fmt.Scan(&authCode); err != nil {
+		log.Fatalf("Unable to read authorization code: %v", err)
 	}
 
-	js, err := json.Marshal(m)
+	tok, err := config.Exchange(context.TODO(), authCode)
+	if err != nil {
+		log.Fatalf("Unable to retrieve token from web: %v", err)
+	}
+	return tok
+}
+
+// Retrieves a token from a local file.
+func tokenFromFile(config *oauth2.Config) (*oauth2.Token, error) {
+	//Get token from bonzai conf
+	c, err := Z.Conf.Data()
 	if err != nil {
 		return nil, err
 	}
-	return []byte(js), err
+	tok := &oauth2.Token{}
+	err = yaml.Unmarshal([]byte(c), tok)
+	//Refresh token if expired
+	if !tok.Valid() {
+		src := config.TokenSource(context.Background(), tok)
+		newToken, err := src.Token()
+		if err != nil {
+			return tok, err
+		}
+		//Check that the new token is different to expired token before
+		//saving to bonzai conf
+		if newToken.AccessToken != tok.AccessToken {
+			saveToken(newToken)
+			return newToken, nil
+		}
+	}
+	return tok, err
+}
+
+// Saves a token to bonzai config
+func saveToken(token *oauth2.Token) {
+	Z.Conf.OverWrite(token)
 }
--- go.mod
+++ go.mod
@@ -5,15 +5,26 @@ go 1.18
 require (
 	github.com/rwxrob/bonzai v0.19.0
 	github.com/rwxrob/conf v0.8.1
+	github.com/rwxrob/emb v0.0.0-20221107055619-d8f499a1e99c
 	github.com/rwxrob/help v0.5.2
 	github.com/rwxrob/vars v0.5.0
+	golang.org/x/oauth2 v0.2.0
+	google.golang.org/api v0.103.0
+	gopkg.in/yaml.v3 v3.0.1
 )
 
 require (
+	cloud.google.com/go/compute v1.12.1 // indirect
+	cloud.google.com/go/compute/metadata v0.2.1 // indirect
 	github.com/a8m/envsubst v1.3.0 // indirect
 	github.com/elliotchance/orderedmap v1.4.0 // indirect
 	github.com/fatih/color v1.13.0 // indirect
 	github.com/goccy/go-yaml v1.9.5 // indirect
+	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/googleapis/enterprise-certificate-proxy v0.2.0 // indirect
+	github.com/googleapis/gax-go/v2 v2.7.0 // indirect
 	github.com/jinzhu/copier v0.3.5 // indirect
 	github.com/magiconair/properties v1.8.6 // indirect
 	github.com/mattn/go-colorable v0.1.12 // indirect
@@ -23,21 +34,25 @@ require (
 	github.com/rwxrob/compcmd v0.3.0 // indirect
 	github.com/rwxrob/compfile v0.1.12 // indirect
 	github.com/rwxrob/fn v0.3.3 // indirect
-	github.com/rwxrob/fs v0.5.2 // indirect
+	github.com/rwxrob/fs v0.8.0 // indirect
 	github.com/rwxrob/pegn v0.0.1 // indirect
-	github.com/rwxrob/scan v0.9.0 // indirect
+	github.com/rwxrob/scan v0.11.0 // indirect
 	github.com/rwxrob/structs v0.6.0 // indirect
 	github.com/rwxrob/term v0.2.8 // indirect
 	github.com/rwxrob/to v0.8.0 // indirect
 	github.com/rwxrob/yq v0.3.0 // indirect
 	github.com/timtadh/data-structures v0.5.3 // indirect
 	github.com/timtadh/lexmachine v0.2.2 // indirect
-	golang.org/x/crypto v0.0.0-20220722155217-630584e8d5aa // indirect
-	golang.org/x/net v0.0.0-20220524220425-1d687d428aca // indirect
-	golang.org/x/sys v0.0.0-20220811171246-fbc7d0a398ab // indirect
-	golang.org/x/term v0.0.0-20220722155259-a9ba230a4035 // indirect
-	golang.org/x/text v0.3.7 // indirect
-	golang.org/x/xerrors v0.0.0-20220517211312-f3a8303e98df // indirect
+	go.opencensus.io v0.24.0 // indirect
+	golang.org/x/crypto v0.1.0 // indirect
+	golang.org/x/net v0.2.0 // indirect
+	golang.org/x/sys v0.2.0 // indirect
+	golang.org/x/term v0.2.0 // indirect
+	golang.org/x/text v0.4.0 // indirect
+	golang.org/x/xerrors v0.0.0-20220907171357-04be3eba64a2 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/genproto v0.0.0-20221027153422-115e99e71e1c // indirect
+	google.golang.org/grpc v1.50.1 // indirect
+	google.golang.org/protobuf v1.28.1 // indirect
 	gopkg.in/op/go-logging.v1 v1.0.0-20160211212156-b2cb9fa56473 // indirect
-	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
